dfbae437e9e64e3e9a8829bfec6eeb21152c8c85
Pietro Scurti <pietroscurti9@gmail.com>
2025-12-10 19:08:59 +0100

    inserita la spiegazione logica

--- script.js
+++ script.js
@@ -13,15 +13,16 @@ const img = document.querySelector(".lampadina")
 let isOn = false
 
 button.addEventListener("click", () => {
-
-
+// se la lampadina è spenta, quando clicco cambia la sorgente dell'immagine, 
+// il valore in true e il testo del bottone in "Spegni"
     if (isOn === false) {
         img.src = "./img/yellow_lamp.png"
         button.innerText = "Spegni"
-
         isOn = true
-
         console.log("accesa");
+
+// altrimenti se la lampadina è accesa, quando clicco rimetti la sorgente dell'immagine precedente, 
+// il valore in false e rimetti il testo "Accendi" nel bottone
     } else {
         img.src = "./img/white_lamp.png"
         button.innerText = "Accendi"
@@ -29,11 +30,12 @@ button.addEventListener("click", () => {
         console.log("spenta");
 
     }
-}
+  }
 );
-
+// quando ho il puntatore sull'elemento button
 button.addEventListener("mouseenter", () => {
-
+// se la lampadina è spenta [isOn === false], modifica lo stile aumentando la luminosità
+// altrimenti diminiuscila
     if (isOn === false) {
         button.style.filter = "brightness(1)";
     } else {
@@ -42,20 +44,21 @@ button.addEventListener("mouseenter", () => {
 
 
     button.addEventListener("mouseleave", () => {
-
+// se la lampadina è accesa [isOn === true], modifica lo stile diminuendo la luminosità
+// altrimenti aumentala
         if (isOn === true) {
             button.style.filter = "brightness(1)";
         } else {
             button.style.filter = "brightness(0.7)";
         }
     }
-    )
-
-
-    console.log("Mouse sopra il bottone");
-
-}
+  )
+ }
 );
+
+
+    
+
 
 
 
